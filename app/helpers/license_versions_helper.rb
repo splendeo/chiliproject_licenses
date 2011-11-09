6493914d50d338dce186e365eaea3adead7daabf
Enrique García Cota <kikito@gmail.com>
2011-11-09 12:26:42 +0100

    working attachments .. in a very cutre way

--- app/helpers/license_versions_helper.rb
+++ app/helpers/license_versions_helper.rb
@@ -1,3 +1,11 @@
 module LicenseVersionsHelper
+  def link_to_attachments(container, options = {})
+    options.assert_valid_keys(:author)
+    
+    if container.attachments.any?
+      options = {:deletable => container.attachments_deletable?, :author => true}.merge(options)
+      render :partial => 'attachments/links', :locals => {:attachments => container.attachments, :options => options}
+    end
+  end
 end
 
