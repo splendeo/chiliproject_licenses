00a471be547245e55984f62ace079ca901635024
Enrique García Cota <kikito@gmail.com>
2011-11-10 13:12:13 +0100

    added license helper methods

--- app/helpers/licenses_helper.rb
+++ app/helpers/licenses_helper.rb
@@ -1,2 +1,13 @@
 module LicensesHelper
+
+  def link_to_license(l)
+    link_to(l.name, get_license_url(l))
+  end
+
+private
+
+  def get_license_version_url(l)
+    return l.url if l.url.present?
+    license_path(l)
+  end
 end
