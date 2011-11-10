bfb3c7515af6fd33669376cbe37122e274482086
Enrique García Cota <kikito@gmail.com>
2011-11-10 19:07:45 +0100

    moved license to sidebar

--- lib/chiliproject_licenses/hooks.rb
+++ lib/chiliproject_licenses/hooks.rb
@@ -2,7 +2,7 @@ module ChiliprojectLicenses
   class Hooks < Redmine::Hook::ViewListener
     # :project
     # :form
-    render_on :view_projects_show_right, :partial => 'projects/license'
+    render_on :view_projects_show_sidebar_bottom, :partial => 'projects/license'
     render_on :view_projects_form, :partial => 'projects/license_fields'
   end
 end
