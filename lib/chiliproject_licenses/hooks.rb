7d5b0f9cffc975d7c28074f0cb872dcf4e8f88b1
Enrique García <kikito@gmail.com>
2011-11-18 12:46:04 +0100

    moved hook views to hook/ subdir. added label for license field

--- lib/chiliproject_licenses/hooks.rb
+++ lib/chiliproject_licenses/hooks.rb
@@ -2,7 +2,7 @@ module ChiliprojectLicenses
   class Hooks < Redmine::Hook::ViewListener
     # :project
     # :form
-    render_on :view_projects_show_sidebar_bottom, :partial => 'projects/license'
-    render_on :view_projects_form, :partial => 'projects/license_fields'
+    render_on :view_projects_show_sidebar_bottom, :partial => 'hooks/projects/license'
+    render_on :view_projects_form, :partial => 'hooks/projects/license_fields'
   end
 end
